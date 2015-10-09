d1ccd55b966c187b1d5482e4bf8cce4cc4035d62
rodolpholl <rodolpholoreto@ig.com.br>
2015-10-09 14:27:20 -0300

    .

--- BakeryManager.Services/Seguranca/CadastroPerfil.cs
+++ BakeryManager.Services/Seguranca/CadastroPerfil.cs
@@ -11,7 +11,7 @@ namespace BakeryManager.Services.Seguranca
     public class CadastroPerfil: BusinessProcessBase, IDisposable
     {
         private PerfilBM perfilBm;
-
+        
         public CadastroPerfil()
         {
             perfilBm = GetObject<PerfilBM>();
