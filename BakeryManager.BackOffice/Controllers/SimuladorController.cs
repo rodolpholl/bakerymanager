56e82a673e0f2a08aff2ea119f4bd8fbc3560ae0
rodolpholl <rodolpholoreto@ig.com.br>
2015-10-31 18:25:33 -0200

    Simulador Concluído

---
+++ BakeryManager.BackOffice/Controllers/SimuladorController.cs
@@ -0,0 +1,92 @@
+﻿using BakeryManager.BackOffice.Models.Cadastros.Produtos;
+using BakeryManager.Services;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryManager.BackOffice.Models.ManterReceita;
+
+namespace BakeryManager.BackOffice.Controllers
+{
+    [Authorize]
+    public class SimuladorController : Controller
+    {
+        // GET: Simulador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ListarCategorias()
+        {
+            using (var simulador = new Simulador())
+            {
+                var listaCategoria = simulador.GetListaCategoria().OrderBy(x => x.Nome).Select(x => new {
+                    x.IdCategoriaProduto,
+                    x.Nome
+                }).ToList();
+
+                return Json(listaCategoria, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult ListarProdutoByCategoria(int IdCategoria)
+        {
+            using (var simulador = new Simulador())
+            {
+                var listaProduto = simulador.GetListaProdutoByCategiria(IdCategoria).OrderBy(x => x.Nome).Select(x => new  {
+                    x.IdProduto,
+                    x.Nome
+                }).ToList();
+
+                return Json(listaProduto, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult ListarFormulaByProduto(int IdProduto)
+        {
+            using (var simulador = new Simulador())
+            {
+                var listaProduto = simulador.GetListaFormulaByCategiria(IdProduto).OrderBy(x => x.Descricao).Select(x => new {
+                    x.IdFormula,
+                    x.Descricao
+                }).ToList();
+
+                return Json(listaProduto, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult GetIngredietesFormula([DataSourceRequest] DataSourceRequest request, int IdFormula)
+        {
+            using (var simulador = new Simulador())
+            {
+                var listaProduto = simulador.GetIngredientesByFormula(IdFormula).Select(x => new IngredienteFormulaModel()
+                {
+                    AGosto = x.AGosto,
+                    Nome = x.Ingrediente.Nome,
+                    Quantidade = x.Quantidade
+                }).ToList();
+
+                return Json(listaProduto.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult SimularReceita ([DataSourceRequest] DataSourceRequest request, int IdFormula, int QtdSimulacao)
+        {
+            using (var simulador = new Simulador())
+            {
+                var listaSimulada = simulador.SimularReceita(IdFormula, QtdSimulacao).Select(x => new IngredienteFormulaModel()
+                {
+                    AGosto = x.AGosto,
+                    Nome = x.Ingrediente.Nome,
+                    Quantidade = x.Quantidade
+                }).ToList();
+
+                return Json(listaSimulada.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ BakeryManager.Services/Simulador.cs
@@ -0,0 +1,76 @@
+﻿using BakeryManager.Entities;
+using BakeryManager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeryManager.Services
+{
+    public class Simulador : BusinessProcessBase, IDisposable
+    {
+
+        private CategoriaProdutoBM categoriaProdutoBm;
+        private ProdutoBM produtoBm;
+        private FormulaBM formulaBm;
+        private FormulaTabelaNutricionalBM formulaTabelaNutricionalBm;
+        private IngredienteFormulaBM ingredienteFormulaBm;
+        public Simulador()
+        {
+            categoriaProdutoBm = GetObject<CategoriaProdutoBM>();
+            produtoBm = GetObject<ProdutoBM>();
+            formulaBm = GetObject<FormulaBM>();
+            formulaTabelaNutricionalBm = GetObject<FormulaTabelaNutricionalBM>();
+            ingredienteFormulaBm = GetObject<IngredienteFormulaBM>();
+        }
+        public void Dispose()
+        {
+            categoriaProdutoBm.Dispose();
+            produtoBm.Dispose();
+            formulaBm.Dispose();
+            formulaTabelaNutricionalBm.Dispose();
+            ingredienteFormulaBm.Dispose();
+        }
+
+
+        public IList<CategoriaProduto> GetListaCategoria()
+        {
+            return categoriaProdutoBm.GetAll();
+        }
+
+        public IList<Produto> GetListaProdutoByCategiria(int idCategoria)
+        {
+            return produtoBm.GetProdutoByCategoria(categoriaProdutoBm.GetByID(idCategoria)).Where(x => x.Ativo).ToList();
+        }
+
+        public IList<Formula> GetListaFormulaByCategiria(int idProduto)
+        {
+            return formulaBm.GetFormulasByProduto(produtoBm.GetByID(idProduto)).Where(x => x.EmUso).ToList();
+        }
+
+        public IList<IngredienteFormula> GetIngredientesByFormula(int idFormula)
+        {
+            return ingredienteFormulaBm.GetByFormula(formulaBm.GetByID(idFormula));
+        }
+
+        public IList<IngredienteFormula> SimularReceita(int idFormula, int qtdSimulacao)
+        {
+
+            var formula = formulaBm.GetByID(idFormula);
+            var fatorConversao = qtdSimulacao / formula.RendimentoPadrao;
+
+            var listaIngredienteFormula = ingredienteFormulaBm.GetByFormula(formula).Select(x => new IngredienteFormula() {
+
+                    AGosto = x.AGosto,
+                    Formula = x.Formula,
+                    IdIngredienteFormula = x.IdIngredienteFormula,
+                    Ingrediente = x.Ingrediente,
+                    Quantidade = Math.Round(x.Quantidade * fatorConversao,2)
+                }).ToList();
+
+            return listaIngredienteFormula;
+
+        }
+    }
+}
