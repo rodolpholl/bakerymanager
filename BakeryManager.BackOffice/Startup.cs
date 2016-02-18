de81b1655ebf9b62837e93360fc6632141b5c143
rodolpholl <rodolpholoreto@ig.com.br>
2016-02-18 14:00:45 -0200

    Install SignalR

---
+++ BakeryManager.BackOffice/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(BakeryManager.BackOffice.Startup))]
+
+namespace BakeryManager.BackOffice
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
