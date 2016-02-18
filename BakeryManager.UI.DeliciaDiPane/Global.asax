dfc741e00ffcc0b980d1e656be056d8b70932384
rodolpholl <rodolpholoreto@ig.com.br>
2016-02-18 17:26:59 -0200

    Criação do Aplicativo do Site

---
+++ BakeryManager.UI.DeliciaDiPane/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BakeryManager.UI.DeliciaDiPane.MvcApplication" Language="C#" %>
---
+++ BakeryManager.UI.DeliciaDiPane/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BakeryManager.UI.DeliciaDiPane.Startup))]
+namespace BakeryManager.UI.DeliciaDiPane
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            return;
+        }
+    }
+}
